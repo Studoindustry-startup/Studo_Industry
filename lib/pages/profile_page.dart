85d90a8a7f2677e8714c76ee087cd73da7befecc
ABHISHEK THORAT <abhithorat.24@gmail.com>
2023-10-30 01:23:12 +0530

    File Uploading #3
    User can Upload File From Phone Storage #3

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,209 @@
+import 'package:chatapp/pages/auth/login_page.dart';
+import 'package:chatapp/pages/home_page.dart';
+import 'package:chatapp/service/auth_service.dart';
+import 'package:chatapp/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:chatapp/helper/helper_function.dart'; // Import the helper function
+
+class ProfilePage extends StatefulWidget {
+  String userName;
+  String email;
+  ProfilePage({Key? key, required this.email, required this.userName})
+      : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  AuthService authService = AuthService();
+  File? _selectedImage;
+
+  Future<void> _selectImage() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+      await _uploadImage(); // Call the upload function after selecting an image
+    }
+  }
+
+  Future<void> _uploadImage() async {
+    try {
+      String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+      firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+          .ref()
+          .child('profile_images')
+          .child('$fileName.jpg');
+
+      await ref.putFile(_selectedImage!);
+      String downloadURL = await ref.getDownloadURL();
+
+      // Save the downloadURL to user's profile
+      await HelperFunctions.saveUserProfileImage(downloadURL);
+
+      // You can now use the downloadURL to display the image or save it to the database.
+    } catch (e) {
+      print("Error uploading image: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+        title: const Text(
+          "Profile",
+          style: TextStyle(
+              color: Colors.white, fontSize: 27, fontWeight: FontWeight.bold),
+        ),
+      ),
+      drawer: Drawer(
+          child: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 50),
+        children: <Widget>[
+          Icon(
+            Icons.account_circle,
+            size: 150,
+            color: Colors.grey[700],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Text(
+            widget.userName,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Divider(
+            height: 2,
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.pop(context); // Close the drawer
+              nextScreen(context, const HomePage());
+            },
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.group),
+            title: const Text(
+              "Groups",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            selected: true,
+            selectedColor: Theme.of(context).primaryColor,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.group),
+            title: const Text(
+              "Profile",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          ListTile(
+            onTap: () async {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Logout"),
+                      content: const Text("Are you sure you want to logout?"),
+                      actions: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            Icons.cancel,
+                            color: Colors.red,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            await authService.signOut();
+                            Navigator.of(context).pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginPage()),
+                                (route) => false);
+                          },
+                          icon: const Icon(
+                            Icons.done,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text(
+              "Logout",
+              style: TextStyle(color: Colors.black),
+            ),
+          )
+        ],
+      )),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 170),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: _selectImage,
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage:
+                    _selectedImage != null ? FileImage(_selectedImage!) : null,
+                child: _selectedImage == null
+                    ? const Icon(
+                        Icons.account_circle,
+                        size: 200,
+                        color: Color.fromARGB(255, 66, 66, 66),
+                      )
+                    : null,
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Full Name", style: TextStyle(fontSize: 17)),
+                Text(widget.userName, style: const TextStyle(fontSize: 17)),
+              ],
+            ),
+            const Divider(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Email", style: TextStyle(fontSize: 17)),
+                Text(widget.email, style: const TextStyle(fontSize: 17)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
