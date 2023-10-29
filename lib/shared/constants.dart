b3aaf52c89021b68f8af3de2529852b3dfd8a2d5
Studo-Industry <studoindustrygithub@gmail.com>
2023-10-29 23:57:30 +0530

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:498899602653:web:3ffdc0d1432c25d4021f34";
+  static String apiKey = "AIzaSyA7Xy8TgGqS5XjK728koJGVYaswb_qhIv8";
+  static String messagingSenderId = "498899602653";
+  static String projectId = "chatappflutter-9d65f";
+  final primaryColor = Color.fromRGBO(243, 153, 32, 1);
+  final secondColor = Color.fromRGBO(57, 185, 182, 1);
+}
